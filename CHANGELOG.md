25dc410b333d9ce28cefcdec68b945cd1fcd82be
Ethan <ethanstephens89@gmail.com>
2020-01-28 14:22:16 -0600

    added version update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## [0.1.0] 
+- Formatting Changes in the dart files
+
 ## [0.0.1] 
 - Initial Version, 1.28.2020
--- lib/curved_drawer.dart
+++ lib/curved_drawer.dart
@@ -28,30 +28,32 @@ class CurvedDrawer extends StatefulWidget {
   //boolean to be used if drawer is used as end drawer
   final bool isEndDrawer;
 
-  CurvedDrawer({
-    Key key,
-    @required this.items,
-    //default select first item
-    this.index = 0,
-    this.color = Colors.white,
-    this.buttonBackgroundColor = Colors.white,
-    this.labelColor = Colors.black54,
-    this.backgroundColor = Colors.transparent,
-    this.onTap,
-    this.animationCurve = Curves.easeOut,
-    this.animationDuration = const Duration(milliseconds: 600),
-    this.width = 75,
-    this.isEndDrawer = false
-  }) : assert(items != null),
-      assert(items.length >= 1),
-      assert(0 <= index && index < items.length),
-      assert(0 <= width && width <= 100),super(key: key);
+  CurvedDrawer(
+      {Key key,
+      @required this.items,
+      //default select first item
+      this.index = 0,
+      this.color = Colors.white,
+      this.buttonBackgroundColor = Colors.white,
+      this.labelColor = Colors.black54,
+      this.backgroundColor = Colors.transparent,
+      this.onTap,
+      this.animationCurve = Curves.easeOut,
+      this.animationDuration = const Duration(milliseconds: 600),
+      this.width = 75,
+      this.isEndDrawer = false})
+      : assert(items != null),
+        assert(items.length >= 1),
+        assert(0 <= index && index < items.length),
+        assert(0 <= width && width <= 100),
+        super(key: key);
 
   @override
   _CurvedDrawerState createState() => _CurvedDrawerState();
 }
 
-class _CurvedDrawerState extends State<CurvedDrawer> with SingleTickerProviderStateMixin {
+class _CurvedDrawerState extends State<CurvedDrawer>
+    with SingleTickerProviderStateMixin {
   double _startingPos;
   int _endingIndex = 0;
   double _pos;
@@ -65,13 +67,13 @@ class _CurvedDrawerState extends State<CurvedDrawer> with SingleTickerProviderSt
   @override
   void initState() {
     super.initState();
-    _items = widget.items.map((item){ 
+    _items = widget.items.map((item) {
       DrawerNavItem(
-        icon: item.icon, 
-        label: item.label, 
-        color: widget.labelColor, 
-        background: widget.buttonBackgroundColor, 
-        size: widget.width/3,
+        icon: item.icon,
+        label: item.label,
+        color: widget.labelColor,
+        background: widget.buttonBackgroundColor,
+        size: widget.width / 3,
       );
     }).toList();
     _icon = _items[widget.index];
@@ -80,8 +82,8 @@ class _CurvedDrawerState extends State<CurvedDrawer> with SingleTickerProviderSt
     _startingPos = _pos;
     _isEnd = widget.isEndDrawer ? -1 : 1;
     _animationController = AnimationController(vsync: this, value: _pos);
-    _animationController.addListener((){
-      setState((){
+    _animationController.addListener(() {
+      setState(() {
         _pos = _animationController.value;
         final endingPos = _endingIndex / widget.items.length;
         final middle = (_startingPos + endingPos) / 2;
@@ -116,67 +118,74 @@ class _CurvedDrawerState extends State<CurvedDrawer> with SingleTickerProviderSt
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-       width: widget.width,
-       color: widget.backgroundColor,
-       child: Stack(
-         overflow: Overflow.visible,
-         alignment: widget.isEndDrawer ? Alignment.centerRight :Alignment.centerLeft,
-         children: <Widget>[
-           Positioned(
-            left: widget.isEndDrawer ? null : -widget.width * 0.6 - (75.0 - widget.width),
-            right: widget.isEndDrawer ? -widget.width * 0.6 - (75.0 - widget.width) : null,
-            top: _pos * size.height,
-            height: size.height / _length,
-            child: Center(
-              child: Transform.translate(
-                offset: Offset(
-                  _isEnd * (1 - _buttonHide) * (75 + (widget.width * 0.2)), 0,
-                ),
-                child: _icon
-              )
+      width: widget.width,
+      color: widget.backgroundColor,
+      child: Stack(
+          overflow: Overflow.visible,
+          alignment:
+              widget.isEndDrawer ? Alignment.centerRight : Alignment.centerLeft,
+          children: <Widget>[
+            Positioned(
+              left: widget.isEndDrawer
+                  ? null
+                  : -widget.width * 0.6 - (75.0 - widget.width),
+              right: widget.isEndDrawer
+                  ? -widget.width * 0.6 - (75.0 - widget.width)
+                  : null,
+              top: _pos * size.height,
+              height: size.height / _length,
+              child: Center(
+                  child: Transform.translate(
+                      offset: Offset(
+                        _isEnd *
+                            (1 - _buttonHide) *
+                            (75 + (widget.width * 0.2)),
+                        0,
+                      ),
+                      child: _icon)),
             ),
-           ),
-           Positioned(
-             top: 0,
-             height: size.height,
-             left: widget.isEndDrawer ? null : 0,
-             right: widget.isEndDrawer ? 0 : null,
-             width:widget.width,
-             child: CustomPaint(
-               painter: DrawerCustomPainter(_pos, _length, widget.color, Directionality.of(context), widget.width, widget.isEndDrawer),
-               child: Container(
-                 width: widget.width,
-                 height: size.height
-               )
-             )
-           ),
-           Positioned(
-            top: 0,
-            bottom: 0,
-            left: widget.isEndDrawer ? null : 0,
-            right: widget.isEndDrawer ? 0 : null,
-            child: SizedBox(
-              width: widget.width * 1.25,
-              child: Column(
-                  children: _items.map((item) {
-                return NavButton(
-                  onTap: _buttonTap,
-                  position: _pos,
-                  length: _length,
-                  isEndDrawer: widget.isEndDrawer,
-                  width: widget.width,
-                  color: widget.labelColor,
-                  index: _items.indexOf(item),
-                  icon: item.icon,
-                );
-              }).toList())),
-          ),
-         ]
-       ),
+            Positioned(
+                top: 0,
+                height: size.height,
+                left: widget.isEndDrawer ? null : 0,
+                right: widget.isEndDrawer ? 0 : null,
+                width: widget.width,
+                child: CustomPaint(
+                    painter: DrawerCustomPainter(
+                        _pos,
+                        _length,
+                        widget.color,
+                        Directionality.of(context),
+                        widget.width,
+                        widget.isEndDrawer),
+                    child:
+                        Container(width: widget.width, height: size.height))),
+            Positioned(
+              top: 0,
+              bottom: 0,
+              left: widget.isEndDrawer ? null : 0,
+              right: widget.isEndDrawer ? 0 : null,
+              child: SizedBox(
+                  width: widget.width * 1.25,
+                  child: Column(
+                      children: _items.map((item) {
+                    return NavButton(
+                      onTap: _buttonTap,
+                      position: _pos,
+                      length: _length,
+                      isEndDrawer: widget.isEndDrawer,
+                      width: widget.width,
+                      color: widget.labelColor,
+                      index: _items.indexOf(item),
+                      icon: item.icon,
+                    );
+                  }).toList())),
+            ),
+          ]),
     );
   }
 
-  void setPage(int index){
+  void setPage(int index) {
     _buttonTap(index);
   }
 
